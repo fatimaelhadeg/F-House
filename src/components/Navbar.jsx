c77e01fb3dfe7e52ef3de6692de06adb01f4ff16
fatimaelhadeg <fatemaelhadeg1@gmail.com>
2025-12-04 13:04:43 +0000

    interfaces

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import { assets } from '../assets/assets'
+import { useState } from "react";
+
+const Navbar = () => {
+    const[showMobile,setShowMobile]=useState(false);
+
+    useEffect(()=>{
+if(showMobile){
+    document.body.style.overflow ='hidden'
+}else{
+    document.body.style.overflow ='auto'
+}
+return()=>{
+    document.body.style.overflow = 'auto'
+}
+    },[showMobile])
+
+    return (
+        <div className='absolute top-0 left-0 w-full z-0'>
+            <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-10 lg:px-16 bg-transparent '>
+                <img src={assets.logo} alt="" />
+                <ul className='hidden md:flex gap-7 text-white'>
+                    <a href="" className="cursor-pointer hover:text-gray-400">Home</a>
+                    <a href="" className="cursor-pointer hover:text-gray-400">About</a>
+                    <a href="" className="cursor-pointer hover:text-gray-400">Projects</a>
+                    <a href="" className="cursor-pointer hover:text-gray-400">Testimonials</a>
+                </ul>
+                <button className="hidden md:block bg-white px-8 py-2 rounded-full">Sign up</button>
+                <img onClick={()=> setShowMobile(true)} src={assets.menu_icon} className='md:hidden w-7 cursor-pointer' alt="" />
+
+            </div>
+            {/*-------------mobile-menu--------*/}
+            <div className={`md:hidden ${showMobile ?'fixed w-full': 'h-0 w-0' } fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
+                <div className='flex justify-end p-6 cursor-pointer'>
+                    <img onClick={()=>setShowMobile(false)} src={assets.cross_icon} alt="" className='w-6' />
+                </div>
+                <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+                    <a  onClick={()=>setShowMobile(false)} href="#home" className='px-4 py-2 rounded-full inline-block'>Home</a>
+                    <a  onClick={()=>setShowMobile(false)} href="about" className='px-4 py-2 rounded-full inline-block'>About</a>
+                    <a  onClick={()=>setShowMobile(false)} href="projects" className='px-4 py-2 rounded-full inline-block'>Projects</a>
+                    <a  onClick={()=>setShowMobile(false)} href="testimonails" className='px-4 py-2 rounded-full inline-block'>Testimonails</a>
+
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
